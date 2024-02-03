bf9ccb783a9e5b73c7f84d88387d3f9a9af66b21
John Conway <jconway46@gatech.edu>
2024-02-03 17:33:51 -0500

    finished pizza menu project

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,15 +1,53 @@
-import { React, StrictMode, createElement } from 'react';
+import { Fragment, React, StrictMode, createElement } from 'react';
 import { createRoot } from 'react-dom/client';
-import pizzaData from './data.js';
+// import pizzaData from './data.js';
 import './index.css'
 
-// {
-//     name: "Focaccia",
-//     ingredients: "Bread with italian olive oil and rosemary",
-//     price: 6,
-//     photoName: "pizzas/focaccia.jpg",
-//     soldOut: false,
-//   },
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
+
 
 function App() {
     return (
@@ -19,17 +57,6 @@ function App() {
             < Footer />
         </div>
     )
-}
-
-function Pizza(props) {
-    return (
-        <div>
-            <img src='pizzas/spinaci.jpg' alt="Pizza Spinaci"></img>
-            <h3>Pizza</h3>
-            <p>something, something, something...</p>
-        </div>
-    )
-
 }
 
 function Header() {
@@ -46,14 +73,61 @@ function Header() {
 
 function Menu() {
 
+    const pizzas = pizzaData;
+    // const pizzas = [];
+
+    // React won't render true/false, but WILL render 0
+    const numPizzas = pizzas.length;
+
     return (
         <main className='menu'>
             <h2>Our Menu:</h2>
-            <Pizza />
-            <Pizza />
-            <Pizza />
-            <Pizza />
+
+
+
+            {numPizzas > 0 ? (
+                <Fragment>
+                    <p>Authentic Italian cuisine.
+                        6 creative dishes to choose from.
+                        All from our stone oven, all organic, all delicious.</p>
+
+                    <ul className='pizzas'>
+                        {pizzas.map((pizza) => (
+                            <Pizza pizzaObject={pizza} key={pizza.name} />
+                        ))}
+                    </ul>
+                </Fragment>
+            ) : <p>We're still working on our menu. Please come back later &#128522;</p>}
+
+
+
+            {/* <Pizza
+                name='Focaccia'
+                ingredients='Bread with italian olive oil and rosemary'
+                price={6}
+                photoName='pizzas/focaccia.jpg'
+                soldOut={false}
+            /> */}
         </main>
+    )
+
+}
+
+function Pizza({ pizzaObject }) {
+
+    // don't even render the pizza if it is sold out
+    // if (pizzaObject.soldOut) return null
+
+    return (
+        // <li className={`pizza ${pizzaObject.soldOut ? 'sold-out' : ''}`}>
+        <li className={pizzaObject.soldOut ? 'pizza sold-out' : 'pizza'}>
+            <img src={pizzaObject.photoName} alt={pizzaObject.name}></img>
+            <div>
+                <h3>{pizzaObject.name}</h3>
+                <p>{pizzaObject.ingredients}</p>
+                <span>{pizzaObject.soldOut ? "Sold Out" : pizzaObject.price}</span>
+            </div>
+        </li>
     )
 
 }
@@ -70,8 +144,35 @@ function Footer() {
     //     alert("We're currently open!");
     // else alert("Sorry, we're closed!");
 
+    // if (!isOpen) {
+    //     return (
+    //         <p>We're happy to welcome you at {openHour}:00 !</p>
+    //     )
+    // } else {
+    return (
+        <footer className='footer'>
+            {
+                isOpen ? (
+                    <Order closeHour={closeHour} />
+                ) : <p>We're happy to welcome you at {openHour}:00 !</p>
+            }
+        </footer >
+    )
+    // }
+
     // return createElement('footer', null, "we're currently open!")
-    return (<footer className='footer'>{new Date().toLocaleTimeString()} We're currently open!</footer>)
+
+}
+
+function Order({ openHour, closeHour }) {
+    return (
+        <div className='order'>
+            <p>
+                We're open until {closeHour}:00. Come visit us or order online.
+            </p>
+            <button className='btn'>Order</button>
+        </div>
+    )
 }
 
 const root = createRoot(document.getElementById("root"));
